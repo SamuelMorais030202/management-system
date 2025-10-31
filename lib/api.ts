3cb0ba4e4c98a909ed1a3090d61c4a32db9ba0de
Samuel Morais030202 <moraissamuel009@gmail.com>
2025-10-31 15:55:32 -0300

    first

--- lib/api.ts
+++ lib/api.ts
@@ -1,8 +1,7 @@
 import axios from 'axios'
 
 const baseURL =
-  process.env.BASE_URL || // ← variável para uso no servidor
-  process.env.NEXT_PUBLIC_BASE_URL // ← variável exposta ao client (fallback)
+  process.env.BASE_URL || "http://179.127.4.150:8095/orion/v1"
 
 export const api = axios.create({
   baseURL,
